6c7e4afe8cad35069e3fc0051c71cde471ca69ce
mattzteh <mattzteh@gmail.com>
2022-11-11 15:40:31 -0800

    Add Cat model, cats table

---
+++ ninety-nine-cats/app/models/cat.rb
@@ -0,0 +1,40 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require 'date'
+class Cat < ApplicationRecord
+    CAT_COLORS = %w(brown black white orange)
+
+    validates :birth_date, presence: true
+    validates :color, presence: true, inclusion: {in: CAT_COLORS, message: 'Color is not in list'}
+    validates :name, presence: true
+    validates :sex, presence: true, inclusion: {in: %w(M F)}
+
+    validate :valid_birth_date
+
+    def valid_birth_date
+        current_date = Date.today
+        if Date.today < birth_date.to_date
+            errors[:birth_date] << "cannot be in the future!"
+        end
+    end
+
+    def age
+        life = Date.today - birth_date.to_date
+        (life / 365).to_i
+    end
+end
+
+
+
+
