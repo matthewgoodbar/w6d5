86ee1fd3bd76a207640ec8f4f8cfea7058503ff1
mattzteh <mattzteh@gmail.com>
2022-11-11 17:34:42 -0800

    Add edit and new actions for cats controller, corresponding views

--- ninety-nine-cats/app/controllers/cats_controller.rb
+++ ninety-nine-cats/app/controllers/cats_controller.rb
@@ -10,15 +10,31 @@ class CatsController < ApplicationController
     end
 
     def edit
+        @cat = Cat.find_by(id: params[:id])    
+        render :edit
     end
 
     def new
+        @cat = Cat.new
+        render :new
     end
 
     def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
     end
     
     def update
+        @cat = Cat.find_by(id: params[:id])
+        if @cat.update(cat_params)
+            redirect_to cat_url(@cat)
+        else
+            render :edit
+        end
     end
 
     private
